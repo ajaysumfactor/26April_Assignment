1b7f74688d274b87b2e4320d680c5456e4e7c5f0
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-04-26 20:18:05 +0530

    q6 updated

--- q6.ts
+++ q6.ts
@@ -1,43 +1,44 @@
 //Write a function that takes a string as input and returns true if the string is a valid palindrome after removing exactly one character, and false otherwise.
-export function canBePalindromeAfterRemove(str : string){
-    if(str.length===0){
-        return false;
-    }
-    let countEach : object = {};
-    for(let i=0; i <str.length ; i++){
-        let ch = str.charAt(i);
-        if(countEach[ch]){
-            countEach[ch]++;
-        }
-        else
-        {
-            countEach[ch]=1;
-        }
-    }
-    //first i have calculate the count of each character
 
-    let odd : number =0;
 
-     for(const key in countEach){
-      if(countEach[key]%2==1){
-        odd++;
-      }
+function isPalindrome(str : string,start : number,end : number): boolean{
+    while(start<end){
+        if(str.charAt(start)!=str.charAt(end)){
+            return false
+        }
+        start++;
+        end--;
     }
-    if(odd>2){
-        return false;
-    }
-    else{
-        return true;
-    }
-   //console.log(countEach);
+    return true;
 }
 
 
-console.log(canBePalindromeAfterRemove("aabbbb"));
-console.log(canBePalindromeAfterRemove("aabbbbnnnnnn"));
-console.log(canBePalindromeAfterRemove("aabbbbvvvz"));
-console.log(canBePalindromeAfterRemove("aabbbbwwweeerrr"));
-console.log(canBePalindromeAfterRemove(""));
-console.log(canBePalindromeAfterRemove("a"));
-console.log(canBePalindromeAfterRemove("zz"));
+
+function canBePalindrome(str : string) : boolean{
+    let start : number =0 ;
+    let end : number =str.length - 1;
+    while(start<end){
+        if(str.charAt(start)==str.charAt(end)){
+            start++;
+            end--;
+        }
+        else{
+            /*if character at start is not equal to character at end . Then two case arises.
+            1.skip start index check palindrome in (start+1---to -----end)
+            2.skip end index check palindrome in (start--to---end-1)
+            */
+            
+
+            return isPalindrome(str,start+1,end) || isPalindrome(str,start,end-1);
+        }
+    }
+    return false;
+}
+
+
+console.log(canBePalindrome("asfgfdsa"));
+console.log(canBePalindrome("asdddddfghlkj"))
+
+ 
+ 
 
