0b818632d5ee641e34054992c7abacae8c7ecb13
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-04-26 22:43:55 +0530

    IPv4 updated

--- q10.ts
+++ q10.ts
@@ -1,7 +1,7 @@
 //10. Write a function that takes a string as input and returns true if it is a valid IPv4 address in dot-decimal notation, and false otherwise.
 
 function isIPv4(str : string):boolean{
-    if(/(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])/.test(str)){
+    if(/^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/.test(str)){
         return true;
     }
     else{
@@ -10,5 +10,8 @@ function isIPv4(str : string):boolean{
   
 }
 
-console.log(isIPv4("192.300.255.255."))
-console.log(isIPv4("192.123.255.255."))
+console.log(isIPv4("192.300.255.255"))
+console.log(isIPv4("192.123.255.255"))
+console.log(isIPv4("123.123.123.123.123"));
+console.log(isIPv4("1213.232.231.2"));
+console.log(isIPv4("125.123.124.129"));
