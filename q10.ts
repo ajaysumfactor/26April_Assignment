5a1c08649a9973d7919bf7bd4eda394320e6ed60
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-04-28 17:06:15 +0530

    implementing logic updated

--- q10.ts
+++ q10.ts
@@ -1,5 +1,6 @@
 //10. Write a function that takes a string as input and returns true if it is a valid IPv4 address in dot-decimal notation, and false otherwise.
 
+
 // function isIPv4(str : string):boolean{
 //     if(/^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/.test(str)){
 //         return true;
@@ -16,7 +17,7 @@ function isIPv4(str : string): boolean{
     let s=str.split('.');
     if(s.length==4){
         for(let i=0;i<s.length;i++){
-            if(s[i][0]=='0'){
+            if(s[i][0]=='0' && s[i].length>1){
                 return false;
             }
             let x=parseInt(s[i],10);
@@ -37,3 +38,4 @@ console.log(isIPv4("123.123.123.123.123"));
 console.log(isIPv4("1213.232.231.2"));
 console.log(isIPv4("125.123.124.129"));
 console.log(isIPv4("05.123.124.129"))
+console.log(isIPv4("123.0.34.234"));
