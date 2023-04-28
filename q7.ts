c3c265bb886b43ec214367644ff033a036cad2e3
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-04-28 14:46:37 +0530

    updated with merge sort logic

--- q7.ts
+++ q7.ts
@@ -1,22 +1,54 @@
 //Write a function that takes two arrays of integers as input and returns a new array containing the common elements of both arrays, sorted in ascending order.
 
- 
+function mergesort(arr): number[] {
+  if (arr.length < 2) {
+    return arr;
 
+  }
+  const middleIndex = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, middleIndex);
+  const rightArr = arr.slice(middleIndex, arr.length);
+  return merge(mergesort(leftArr), mergesort(rightArr));
 
-export function commonOfBothArray(arr1 : number[],arr2 : number[]){
-    let s=new Set();
-    for(const element of arr1){
-       if(arr2.includes(element)){
-        s.add(element);
-       }
+}
+function merge(leftArr, rightArr): number[] {
+  let resultarr: number[] = [];
+  let leftindex = 0;
+  let rightindex = 0;
+  while (leftindex < leftArr.length && rightindex < rightArr.length) {
+    if (leftArr[leftindex] < rightArr[rightindex]) {
+      resultarr.push(leftArr[leftindex]);
+      leftindex = leftindex + 1;
     }
-  let finalArray =Array.from(s);
-   finalArray.sort((a :any,b:any)=>a-b);
-    return finalArray 
+    else {
+      resultarr.push(rightArr[rightindex]);
+      rightindex = rightindex + 1;
+    }
+  }
+  return resultarr.concat(leftArr.slice(leftindex)).concat(rightArr.slice(rightindex));
+
 }
 
 
-console.log(commonOfBothArray([4,3,2,5,6,1,8,9],[5,4,2,6,1]));
+
+
+
+
+export function commonOfBothArray(arr1: number[], arr2: number[]) {
+  let s = new Set();
+  for (const element of arr1) {
+    if (arr2.includes(element)) {
+      s.add(element);
+    }
+  }
+  let finalArray = Array.from(s);
+
+  return mergesort(finalArray);
+}
+
+
+console.log(commonOfBothArray([4, 3, 2, 5, 6, 1, 8, 9], [5, 4, 2, 6, 1]));
+console.log(commonOfBothArray([10,23,34,54,21,11,2,1,34,65],[23,54,11,2,34,11,1,22]));
 
 
 
@@ -24,37 +56,4 @@ console.log(commonOfBothArray([4,3,2,5,6,1,8,9],[5,4,2,6,1]));
 
 
 
-
-// function commonOfBothArray(arr1 : number[],arr2 : number[]) : number[]{
-
-//     let ptr1 : number=0;
-//     let ptr2 : number=0;
- 
-//     let lengthArray1 : number =arr1.length;
-//     let lengthArray2 : number =arr2.length;
- 
-//     let resultArray: number[]=[];
- 
-//     while(ptr1<lengthArray1 && ptr2<lengthArray2){
- 
-//      if(arr1[ptr1] < arr2[ptr2]){
-//          ptr1++
-//      }
-//      else if(arr1[ptr1] > arr2[ptr2]){
-//          ptr2++;
-//      }
-//      else{
-//          resultArray.push(arr1[ptr1]);
-//          ptr1++;
-//          ptr2++
- 
-//      }
-//     }
- 
-//     return resultArray;
-//  }
- 
- 
-//  console.log(commonOfBothArray([1,2,3,4,5,6,7],[2,3,4,4,5]));
- 
 
