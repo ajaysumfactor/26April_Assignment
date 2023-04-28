35b28a3d26609b160d038aa220f0c1e8dfb96b41
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-04-28 15:13:00 +0530

    implementing logic other than regex

--- q10.ts
+++ q10.ts
@@ -1,13 +1,34 @@
 //10. Write a function that takes a string as input and returns true if it is a valid IPv4 address in dot-decimal notation, and false otherwise.
 
-function isIPv4(str : string):boolean{
-    if(/^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/.test(str)){
+// function isIPv4(str : string):boolean{
+//     if(/^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/.test(str)){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }  
+  
+// }
+
+
+
+function isIPv4(str : string): boolean{
+    let s=str.split('.');
+    if(s.length==4){
+        for(let i=0;i<s.length;i++){
+            if(s[i][0]=='0'){
+                return false;
+            }
+            let x=parseInt(s[i],10);
+            // console.log(x);
+            if( isNaN(x) || (x<0 || x>255)){
+                return false;
+            }
+        }
         return true;
     }
-    else{
-        return false;
-    }  
-  
+    return false;
+    
 }
 
 console.log(isIPv4("192.300.255.255"))
@@ -15,3 +36,4 @@ console.log(isIPv4("192.123.255.255"))
 console.log(isIPv4("123.123.123.123.123"));
 console.log(isIPv4("1213.232.231.2"));
 console.log(isIPv4("125.123.124.129"));
+console.log(isIPv4("05.123.124.129"))
